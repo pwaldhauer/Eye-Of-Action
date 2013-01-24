cc09e5f056b22f61141e0ac7991cabb2567525cd
Marcel Wichmann <UAIRRR@Marcels-MacBook-Air.local>
2013-01-24 22:05:26 +0100

    Initial Commit

---
+++ Ball3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ball3
+//
+//  Created by Marcel Wichmann on 1/24/13.
+//  Copyright (c) 2013 Marcel Wichmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Ball3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ball3
+//
+//  Created by Marcel Wichmann on 1/24/13.
+//  Copyright (c) 2013 Marcel Wichmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
